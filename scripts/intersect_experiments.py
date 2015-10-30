a8abfd8a50b3101f449b8dc2d372b15d6474d618
Tony Chun <stonyc@umich.edu>
2015-10-30 15:07:08 -0400

    ADDED: test + scripts
    + scripts/intersect_experiments.py
    + test/Homo_sapiens.GRCh38.78.test.gtf
    + test/isoforms-test.fpkm_tracking
    + test/test.bam

---
+++ scripts/intersect_experiments.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+# DESCRIPTION: Merge multiple SPECtre analysis output files into a single
+# table, based on common transcripts.
+
+# USAGE: python intersect_experiments.py <comma-delimited-list-of-spectre-output-files> > specte_merged.txt
